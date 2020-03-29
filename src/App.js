15985bcc6492a925ccc99534d4c0bac5b5a193f2
Wes <wal2.designer@gmail.com>
2020-03-29 12:05:37 -0300

    estrutura pronta

--- src/App.js
+++ src/App.js
@@ -12,8 +12,55 @@ class App extends Component {
       <div>
         <Container fixed maxWidth= 'lg'>
         <div className="container container--line">
-            <Svg gArea='3 / 2 / 2 / auto '></Svg>
-            <Svg pt='0,25 75, 25 75, 25 75,125' gArea='2 / 2 / auto / 2'></Svg>
+            <Svg pt='0,25 75, 25 75, 25 75,110' gArea='1 / 2 / 4 / 2'></Svg>{/*esquerda para baixo 1/2 1 1/2 */}
+           
+            <Svg pt='0, 25 200, 25' gArea='3 / 4 / auto / 6'></Svg>{/*esquerda para centro 1 1/2 */}
+            <Svg pt='0 425,335 425, 0' gArea='18 / 6 / 9 / 9'></Svg>{/*esquerda para centro 1 1/2 */}
+            <Svg pt='0 525,350 525, 350 25, 295 25, 25' gArea='20 / 6 / 9 / 9'></Svg>{/*esquerda para centro 1 1/2 */}
+            <Svg gArea='21 / 4 / auto / auto'></Svg>{/*esquerda para centro 1 1/2 */}
+            <Svg gArea='21 / 6 / auto / 9'></Svg>{/*esquerda para centro 1 1/2 */}
+            <Svg gArea='19 / 4'></Svg>{/*esquerda para centro 1 1/2 */}
+
+           
+            <Svg pt='300, 25 90, 25' gArea='3 / 5 / auto / 7'></Svg>{/*direita para centro 1 1/2 */}
+           
+            <Svg pt ='140 25,14 25, 0' gArea='3 / 8'></Svg>{/*direita para esquerda 1 */}
+            <Svg pt ='140 25,14 25, 0' gArea='7 / 4'></Svg>{/*direita para esquerda 1 */}
+            <Svg pt ='140 25,14 25, 0' gArea='9 / 6'></Svg>{/*direita para esquerda 1 */}
+            <Svg pt ='140 25,14 25, 0' gArea='17 / 4'></Svg>{/*direita para esquerda 1 */}
+           
+            <Svg gArea='3 / 2'></Svg>{/*esquerda para direita 1*/}
+           
+            <Svg pt='150 25,75 25,75 110, 0' gArea='1 / 8 / 4 / auto'></Svg>{/*direira para baixo 1/2 1 1/2 */}
+            <Svg pt='150 25,75 25,75 225,125 225, 0' gArea='15 / 2 / 20/ auto '></Svg>{/*direira para baixo 1/2 1 1/2 */}
+
+           
+            <Svg pt='75 0,75 35, 0' gArea='2 / 7'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='6 / 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='8 / 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='10 / 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='12/ 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='14 / 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='16 / 5'></Svg>{/*vertical 1*/}
+            <Svg pt='75 0,75 35, 0' gArea='18 / 3'></Svg>{/*vertical 1*/}
+
+            <Svg pt='75 50,75 14, 0' gArea='16 / 3'></Svg>{/*vertical 1*/}
+
+
+           
+            <Svg pt='75 25,75 85, 0' gArea='3 / 5 / 5 / auto '></Svg>{/*vertical centro 1 1/2 */}
+
+            <Svg pt='75 150,75 25,270 25, 0' gArea='7 / 3 / 5 / 5'></Svg>{/*cima para esquerda 1/2 1 1/2 */}
+            <Svg pt='75 300,75 25,270 25, 0' gArea='15 / 3 / 9 / 5'></Svg>{/*cima para esquerda 1/2 1 1/2 */}
+
+            <Svg pt='0 75,210 75,210 15,0' gArea='12 / 6 / 10 / 8'></Svg>{/*cima para esquerda 1/2 1 1/2 */}
+
+            <Svg pt='350 0,350 25,75 25,75 35,0' gArea='20 / 6 / 21 / 3'></Svg>{/*cima para esquerda 1/2 1 1/2 */}
+
+
+            
+
+
         </div>
         <div className="container container--box">
           {data.map((conteudos, index) => {
--- src/components/svg.jsx
+++ src/components/svg.jsx
@@ -11,11 +11,32 @@ class Svg extends Component {
   render() {
     return (
       <svg className="svg" style={{ gridArea: this.props.gArea }}>
+        <defs>
+          <marker
+            id="arrow"
+            markerWidth="10"
+            markerHeight="10"
+            refX="0"
+            refY="3"
+            orient="auto"
+            markerUnits="strokeWidth"
+            viewBox="0 0 20 20"
+          >
+            <path d="M0,0 L0,6 L9,3 z" fill="#f00" />
+          </marker>
+        </defs>
+        <marker id="circle" markerWidth="4" markerHeight="4" refX="2" refY="2">
+          <circle cx="2" cy="2" r="2" stroke="none" fill="#f00" />
+        </marker>
+
         <polyline
-          points={this.props.pt == null ? ("0, 25 1280,25") : this.props.pt}
+          points={this.props.pt == null ? "0, 25 125,25" : this.props.pt}
           fill="none"
           stroke="black"
-          stroke-width="3"
+          stroke-width="2"
+          marker-end="url(#arrow)"
+          marker-start="url(#circle)"
+          // marker-mid="url(#circle)"
         />
       </svg>
     );
